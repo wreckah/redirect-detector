a64192a5dfaf4646e31c613d1b86ecbc1f9cfce8
Alexander Pokatilov <wreckah@ya.ru>
2020-06-02 11:31:06 +0300

    Handle errors, return a proper exit code

--- redirect_detector/__main__.py
+++ redirect_detector/__main__.py
@@ -1,8 +1,14 @@
-from sys import argv
+import sys
 from .detector import detect
+from .errors import UrlDetectorError
 
 
-if len(argv) > 1:
-    print(detect(argv[-1]))
+if len(sys.argv) > 1:
+    try:
+        sys.stdout.write(detect(sys.argv[-1]))
+    except Exception as err:
+        sys.stderr.write(str(err))
+        exit(1)
 else:
-    print('Usage: python3 -m redirect-detector http://w3c.org')
+    sys.stdout.write('Usage: python3 -m redirect-detector http://w3c.org')
+    exit(1)
