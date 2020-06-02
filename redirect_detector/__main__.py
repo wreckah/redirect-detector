09537a3c0d04eaf8bff56c4457532ba259e0b4c4
Alexander Pokatilov <wreckah@ya.ru>
2020-06-02 11:37:46 +0300

    Fix status code

--- redirect_detector/__main__.py
+++ redirect_detector/__main__.py
@@ -8,7 +8,7 @@ if len(sys.argv) > 1:
         sys.stdout.write(detect(sys.argv[-1]))
     except Exception as err:
         sys.stderr.write(str(err))
-        exit(1)
+        sys.exit(1)
 else:
     sys.stdout.write('Usage: python3 -m redirect-detector http://w3c.org')
-    exit(1)
+    sys.exit(1)
